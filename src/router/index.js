a89e00274f9b8ce6f21bca4728a372e537f3ea43
Anna-fog <anna49.fog@yandex.ru>
2026-08-16 17:35:43 +0300

    Use createWebHashHistory for router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import MainPage from '@/views/MainPage.vue'
 import CoursesPage from '@/views/CoursesPage.vue'
 import ContactsPage from '@/views/ContactsPage.vue'
 import PublicOfferPage from '@/views/PublicOfferPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
